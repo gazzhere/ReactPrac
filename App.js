a24c27d44900cd5d7150ae068c3dc9cce6877b38
ganesh <gzesingh1432@gmail.com>
2023-12-17 12:21:24 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,19 @@
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "tag" }, "i am an h1 tag"),
+//     React.createElement("h1", { id: "tag" }, "i am an h1 tag"),
+//     React.createElement("h1", { id: "tag" }, "i am an h1 tag"),
+//   ])
+// );
+
+// // const heading = React.createElement(
+// //   "h1", 
+// //   { id: "heading" },
+// //   "hello from react"
+// // );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+// console.log(parent);
+
